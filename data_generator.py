98385d80dce7ca2b9dc4fd9ed43d9aaa55d58ef8
koder aka kdanilov <kdanilov@mirantis.com>
2015-02-04 17:09:28 +0200

    fix itertools.production usage

--- data_generator.py
+++ data_generator.py
@@ -7,7 +7,7 @@ from petname import Generate as pet_generate
 from storage_api import create_storage
 
 
-types = ["GA", "master"] + [pet_generate(2, '-') for _ in range(10)]
+types = ["GA", "master"] + [pet_generate(2, '-') for _ in range(2)]
 random.shuffle(types)
 tp = itertools.cycle(types)
 
@@ -16,14 +16,15 @@ op_type = ["randread", "read", "randwrite", "write"]
 is_sync = ["s", "a"]
 
 storage = create_storage(sys.argv[1])
+combinations = list(itertools.product(op_type, is_sync, sz))
 
 for i in range(30):
     row = {"build_id": pet_generate(2, " "),
            "type": next(tp),
            "iso_md5": uuid.uuid4().get_hex()}
 
-    for sz, op_type, is_sync in itertools.product(op_type, is_sync, sz):
-        row[" ".join([sz, op_type, is_sync])] = (random.random(),
-                                                 random.random() / 5)
+    for sz, op_type, is_sync in combinations:
+        row[" ".join([sz, op_type, is_sync])] = (random.random() * 100,
+                                                 random.random() * 5)
 
     storage.store(row)

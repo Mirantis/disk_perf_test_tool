674dba19db0547262c48006c1eaa158bd6198880
Yulia Portnova <yportnova@mirantis.com>
2015-12-14 10:20:51 +0200

    fixes

--- wally/suits/omgbench/prepare.sh
+++ wally/suits/omgbench/prepare.sh
@@ -2,23 +2,20 @@
 
 set -e
 set -x
-#
-#OMGPATN=/tmp
-#
-#mkdir -p "$OMGPATN"
-#cd "$OMGPATN"
-#
-#git clone https://github.com/openstack/rally
-#git clone https://github.com/Yulya/omgbenchmark
-#
-#mkdir venv
-#cd rally
-#./install_rally.sh -d "$OMGPATN"/venv -y
-#
-#cd "$OMGPATN"
-#source venv/bin/activate
-#apt-get -y install python-scipy libblas-dev liblapack-dev libatlas-base-dev gfortran
-#pip install oslo.messaging petname scipy
-#
-#
-#
+
+OMGPATN=/tmp
+
+mkdir -p "$OMGPATN"
+cd "$OMGPATN"
+
+git clone https://github.com/openstack/rally
+git clone https://github.com/Yulya/omgbenchmark
+
+mkdir venv
+cd rally
+./install_rally.sh -d "$OMGPATN"/venv -y
+
+cd "$OMGPATN"
+source venv/bin/activate
+apt-get -y install python-scipy libblas-dev liblapack-dev libatlas-base-dev gfortran
+pip install oslo.messaging petname scipy
--- wally/suits/omgbench/run.sh
+++ wally/suits/omgbench/run.sh
@@ -31,9 +31,9 @@ source venv/bin/activate
 
 cd omgbenchmark/rally_plugin
 
-#sed -i -e "s,rabbit:\/\/guest:guest@localhost\/,$URL,g" deployment.json
-#sed -i -e "s,times\": 100,times\": $TIMES,g" task.json
+sed -i -e "s,rabbit:\/\/guest:guest@localhost\/,$URL,g" deployment.json
+sed -i -e "s,times\": 100,times\": $TIMES,g" task.json
 
-#rally --plugin-paths . deployment create --file=deployment.json --name=test2 &> /dev/null
+rally --plugin-paths . deployment create --file=deployment.json --name=test2 &> /dev/null
 rally --plugin-paths . task start task.json &> ~/omg.log
 cat ~/omg.log | grep  "Load duration" | grep -o '[0-9,.]\+'
